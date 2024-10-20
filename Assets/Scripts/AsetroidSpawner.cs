43d629bae159162082bb08ff1d443888377ea051
NullPerson <edu1khoory@hotmail.com>
2024-10-20 10:37:08 +0400

    Added Hardmode and Dashing

--- Assets/Scripts/AsetroidSpawner.cs
+++ Assets/Scripts/AsetroidSpawner.cs
@@ -11,17 +11,31 @@ public class AsetroidSpawner : MonoBehaviour
     [SerializeField] private float amplitude;
     [SerializeField] private float speed;
 
-
+    [SerializeField] private float hardModeTimer;
+    [SerializeField] private float hSpawnDuration;
+    [SerializeField] private bool hardMode;
 
     // Start is called before the first frame update
     void Start()
     {
+        hardMode = false;
         timeHolder = spawnDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (!hardMode) 
+        hardModeTimer -= Time.deltaTime;
+
+
+        if (hardModeTimer <= 0)
+        {
+            hardMode = true;
+            timeHolder = hSpawnDuration;
+        }
+
 
         float sin = Mathf.Sin(Time.time * speed) * amplitude;
 
@@ -31,6 +45,7 @@ public class AsetroidSpawner : MonoBehaviour
         if (spawnDuration <= 0)
         {
             StartCoroutine(SpawnAsetroid());
+            if (timeHolder < hSpawnDuration) timeHolder = hSpawnDuration;
             spawnDuration = timeHolder;
 
         }
@@ -43,5 +58,10 @@ public class AsetroidSpawner : MonoBehaviour
         Instantiate(asetroid, transform.position, Quaternion.identity);
 
 
+    }
+
+    IEnumerator MakeGameHarder()
+    {
+        yield return new WaitForSeconds(timeHolder);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private Rigidbody2D _rb2D;
     [SerializeField] private float speed;
+    [SerializeField] private float dashSpeed;
     [SerializeField] private GameObject placeholder;
     [SerializeField] private GameObject bullet;
     [SerializeField] private bool isMoving;
     [SerializeField] private float bulletTimer;
+    [SerializeField] private float dashTimer;
     private float timerHolder;
 
     [SerializeField] private static float direction;
@@ -30,21 +32,30 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         bulletTimer -= Time.deltaTime;
+        dashTimer -= Time.deltaTime;
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
         if (xAxis != 0 || yAxis != 0)
         {
             isMoving = true;
-            direction = Mathf.Sqrt(xAxis * xAxis + yAxis * yAxis);
+            direction = Mathf.Sqrt(xAxis * xAxis + yAxis * yAxis); //for drawing gizmos
         }
         else
         {
             isMoving = false;
         }
 
-        _rb2D.velocity = new Vector2(xAxis * speed, yAxis * speed);
+        if (Input.GetKey(KeyCode.LeftShift) && dashTimer <= 0)
+        {
+            StartCoroutine(Dash());
+            dashTimer = timerHolder;
+        }
+        else
+        {
+            _rb2D.velocity = new Vector2(xAxis * speed, yAxis * speed);
+        }
 
-        if (Input.GetKeyDown(KeyCode.Space) && bulletTimer <= 0)
+        if (Input.GetKey(KeyCode.Space) && bulletTimer <= 0)
         {
             Instantiate(bullet, transform.position, Quaternion.identity);
             bulletTimer = timerHolder;
@@ -66,6 +77,13 @@ public class PlayerMovement : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + direction, transform.position.y + direction));
+    }
+
+    IEnumerator Dash()
+    {
+        //_rb2D.velocity = new Vector2(_rb2D.velocity.x * dashSpeed, _rb2D.velocity.y * dashSpeed);
+        _rb2D.AddForce(new Vector2(_rb2D.velocity.x * dashSpeed, _rb2D.velocity.y * dashSpeed));
+        yield return new WaitForSeconds(0.2f);
     }
 
 }
